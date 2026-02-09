fa45404216c4b8d49b95f52a16e96abc4bee17c5
RataJui01 <itmonero@gmail.com>
2026-02-09 07:15:53 +0700

    Setting up project

---
+++ personal-expense-tracker/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-amber-500 p-3">
+      <h1 className="p-3 text-6xl font-bold text-red-700">Hello React</h1>
+    </div>
+  );
+}
+
+export default App;
